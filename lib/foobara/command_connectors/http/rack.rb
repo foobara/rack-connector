28e3ae8e2598fff836e36b888ceba3df7488cd50
Miles Georgi <azimux@gmail.com>
2023-10-08 15:03:53 -0700

    Extract Context class from rack connector

--- lib/foobara/command_connectors/http/rack.rb
+++ lib/foobara/command_connectors/http/rack.rb
@@ -3,34 +3,18 @@ module Foobara
     class Http < Foobara::CommandConnector
       class Rack < Http
         def call(env)
-          # PATH_INFO has the path
-          # REQUEST_URI has whole uri
-          # QUERY_STRING has the query string
-          # REQUEST_METHOD is the request method
-          # rack.errors stream?
-          # rack.input stream?
-          # HTTP_ is all the headers
+          request = run(env)
+          response = request.response
+          [response.status, response.headers, [response.body]]
+        rescue NoCommandFoundError, InvalidContextError => e
+          [404, {}, [e.message]]
+        rescue => e
+          # :nocov:
+          env["rack.errors"].puts e.to_s
+          env["rack.errors"].puts e.backtrace
 
-          path = env["PATH_INFO"]
-          query_string = env["QUERY_STRING"]
-          method = env["REQUEST_METHOD"]
-          body = env["rack.input"].read
-          headers = env.select { |s| s.start_with?("HTTP_") }
-
-          begin
-            request = run(path:, method:, headers:, query_string:, body:)
-            response = request.response
-            [response.status, response.headers, [response.body]]
-          rescue NoCommandFoundError, InvalidContextError => e
-            [404, {}, [e.message]]
-          rescue => e
-            # :nocov:
-            env["rack.errors"].puts e.to_s
-            env["rack.errors"].puts e.backtrace
-
-            raise e
-            # :nocov:
-          end
+          raise e
+          # :nocov:
         end
       end
     end
