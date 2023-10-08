3e83c955f09e40c9e5d943209c9db318d7bc61f9
Miles Georgi <azimux@gmail.com>
2023-10-08 15:03:53 -0700

    100

---
+++ lib/foobara/command_connectors/http/rack/context.rb
@@ -0,0 +1,30 @@
+module Foobara
+  module CommandConnectors
+    class Http < Foobara::CommandConnector
+      class Rack < Http
+        class Context < Http::Context
+          attr_accessor :env
+
+          def initialize(env)
+            self.env = env
+
+            # PATH_INFO has the path
+            # REQUEST_URI has whole uri
+            # QUERY_STRING has the query string
+            # REQUEST_METHOD is the request method
+            # rack.errors stream?
+            # rack.input stream?
+            # HTTP_ is all the headers
+            super(
+              path: env["PATH_INFO"],
+              query_string: env["QUERY_STRING"],
+              method: env["REQUEST_METHOD"],
+              body: env["rack.input"].read,
+              headers: env.select { |s| s.start_with?("HTTP_") },
+            )
+          end
+        end
+      end
+    end
+  end
+end
